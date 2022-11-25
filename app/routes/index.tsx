af354215bf0a80f1f1599adcfb08295a7bb1a350
Sylvain DENYSE <sylvain.denyse@gmail.com>
2022-11-25 10:33:45 +0100

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,60 @@
-export default function Index() {
+import QRCode from 'qrcode'
+
+type PuzzleApi = {
+  id: string
+  slug: string
+  title: string
+}
+
+const puzzles: Array<PuzzleApi> = [
+  {
+    id: 'puzzles-001',
+    slug: 'slug-puzzles-001',
+    title: 'This is the first puzzle',
+  },
+  {
+    id: 'puzzles-002',
+    slug: 'slug-puzzles-002',
+    title: 'This is the second puzzle',
+  },
+]
+
+const generateQr = async (slug: string) => {
+  try {
+    return await QRCode.toDataURL(slug)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const Index = () => {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Puzzles list</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {puzzles.map(puzzle => (
+          <li key={puzzle.id}>
+            <Puzzle {...puzzle} />
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
+
+type PuzzleProps = PuzzleApi
+const Puzzle = ({ slug, title }: PuzzleProps) => {
+  const src = generateQr(slug)
+  const alt = `QR Code of "${title}" puzzle`
+
+  return (
+    <article>
+      <img src={src} height={100} width={100} alt={alt} />
+      <p>
+        <a href={slug}>{title}</a>
+      </p>
+    </article>
+  )
+}
+
+export default Index
