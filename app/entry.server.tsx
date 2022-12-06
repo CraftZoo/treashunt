00f8b7167cdffba99513d831426a22cd751cdfe9
netoun <coulonnier.nicolas.44@gmail.com>
2022-12-06 18:35:16 +0100

    setup chakra

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,19 +1,23 @@
 import { PassThrough } from 'stream'
+
 import type { EntryContext } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import isbot from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
+import { CacheProvider as EmotionCacheProvider } from '@emotion/react'
+import createEmotionServer from '@emotion/server/create-instance'
+import { defaultCache } from './createEmotionCache'
 
 const ABORT_DELAY = 5000
 
-export default function handleRequest(
+const handleRequest = (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
-  return isbot(request.headers.get('user-agent'))
+) =>
+  isbot(request.headers.get('user-agent'))
     ? handleBotRequest(
         request,
         responseStatusCode,
@@ -26,38 +30,44 @@ export default function handleRequest(
         responseHeaders,
         remixContext
       )
-}
+export default handleRequest
 
-function handleBotRequest(
+const handleBotRequest = (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
-  return new Promise((resolve, reject) => {
+) =>
+  new Promise((resolve, reject) => {
     let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
+      <EmotionCacheProvider value={defaultCache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </EmotionCacheProvider>,
       {
-        onAllReady() {
-          const body = new PassThrough()
+        onAllReady: () => {
+          const reactBody = new PassThrough()
+          const emotionServer = createEmotionServer(defaultCache)
+
+          const bodyWithStyles = emotionServer.renderStylesToNodeStream()
+          reactBody.pipe(bodyWithStyles)
 
           responseHeaders.set('Content-Type', 'text/html')
 
           resolve(
-            new Response(body, {
+            new Response(bodyWithStyles, {
               headers: responseHeaders,
               status: didError ? 500 : responseStatusCode,
             })
           )
 
-          pipe(body)
+          pipe(reactBody)
         },
-        onShellError(error: unknown) {
+        onShellError: (error: unknown) => {
           reject(error)
         },
-        onError(error: unknown) {
+        onError: (error: unknown) => {
           didError = true
 
           console.error(error)
@@ -67,38 +77,43 @@ function handleBotRequest(
 
     setTimeout(abort, ABORT_DELAY)
   })
-}
 
-function handleBrowserRequest(
+const handleBrowserRequest = (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
-  return new Promise((resolve, reject) => {
+) =>
+  new Promise((resolve, reject) => {
     let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
+      <EmotionCacheProvider value={defaultCache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </EmotionCacheProvider>,
       {
-        onShellReady() {
-          const body = new PassThrough()
+        onShellReady: () => {
+          const reactBody = new PassThrough()
+          const emotionServer = createEmotionServer(defaultCache)
+
+          const bodyWithStyles = emotionServer.renderStylesToNodeStream()
+          reactBody.pipe(bodyWithStyles)
 
           responseHeaders.set('Content-Type', 'text/html')
 
           resolve(
-            new Response(body, {
+            new Response(bodyWithStyles, {
               headers: responseHeaders,
               status: didError ? 500 : responseStatusCode,
             })
           )
 
-          pipe(body)
+          pipe(reactBody)
         },
-        onShellError(err: unknown) {
-          reject(err)
+        onShellError: (error: unknown) => {
+          reject(error)
         },
-        onError(error: unknown) {
+        onError: (error: unknown) => {
           didError = true
 
           console.error(error)
@@ -108,4 +123,3 @@ function handleBrowserRequest(
 
     setTimeout(abort, ABORT_DELAY)
   })
-}
