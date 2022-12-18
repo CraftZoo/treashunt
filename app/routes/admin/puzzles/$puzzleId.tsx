5563b7ed77a64390452b1069aefcc69c85d5d435
Sylvain DENYSE <sylvain.denyse@gmail.com>
2022-12-18 20:21:14 +0100

    feat: add page titles

--- app/routes/admin/puzzles/$puzzleId.tsx
+++ app/routes/admin/puzzles/$puzzleId.tsx
@@ -1,4 +1,4 @@
-import type { LoaderArgs } from '@remix-run/node'
+import type { LoaderArgs, MetaFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 
@@ -6,6 +6,18 @@ import PuzzleUpdate from '~/components/organisms/PuzzleUpdate'
 import { getPuzzle } from '~/models/puzzle.server'
 
 export { action } from '~/components/organisms/PuzzleUpdate'
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (!data)
+    return {
+      title: 'Énigme inconnue · TreasHunt',
+    }
+
+  const { puzzle } = data
+  return {
+    title: `Modifier l'énigme "${puzzle.title}" · TreasHunt`,
+  }
+}
 
 export const loader = async ({ params }: LoaderArgs) => {
   const { puzzleId } = params
--- app/routes/admin/puzzles/add.tsx
+++ app/routes/admin/puzzles/add.tsx
@@ -1,6 +1,12 @@
+import type { MetaFunction } from '@remix-run/node'
+
 import PuzzleCreation from '~/components/organisms/PuzzleCreation'
 
 export { action } from '~/components/organisms/PuzzleCreation'
+
+export const meta: MetaFunction = () => ({
+  title: 'Ajouter une énigme · TreasHunt',
+})
 
 const AddPuzzleRoute = () => {
   return <PuzzleCreation />
